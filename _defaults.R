7cbee897413efb52adbc92130cbc62161e29a414
JoFrhwld <JoFrhwld@gmail.com>
2023-06-02 12:55:04 -0400

    adding ggplot2 defaults

--- _defaults.R
+++ _defaults.R
@@ -5,6 +5,14 @@ theme_set(
   )
 )
 
+options(
+  ggplot2.discrete.colour = khroma::scale_color_bright,
+  ggplot2.discrete.fill = khroma::scale_fill_bright,
+  ggplot2.continuous.colour = khroma::scale_color_batlow,
+  ggplot2.continuous.fill = khroma::scale_fill_batlow
+)
+
 ragg_png <- function(..., res = 150) {
   ragg::agg_png(..., res = res, units = "in")
 }
+
