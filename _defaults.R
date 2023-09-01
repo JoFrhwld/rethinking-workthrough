31673eb5e608fc7e3df039491ae2def994169103
JoFrhwld <JoFrhwld@gmail.com>
2023-09-01 17:47:34 -0400

    updating defaults

--- _defaults.R
+++ _defaults.R
@@ -1,7 +1,8 @@
 theme_set(
   theme_minimal(base_size = 16) +
   theme(
-    panel.grid.minor = element_blank()
+    panel.grid.minor = element_blank(),
+    aspect.ratio = 2/(1+sqrt(5))
   )
 )
 
@@ -18,7 +19,9 @@ options(
   ggplot2.discrete.colour = khroma::scale_color_bright,
   ggplot2.discrete.fill = khroma::scale_fill_bright,
   ggplot2.continuous.colour = khroma::scale_color_batlow,
-  ggplot2.continuous.fill = khroma::scale_fill_batlow
+  ggplot2.continuous.fill = khroma::scale_fill_batlow,
+  ggplot2.ordinal.colour = \(...) rcartocolor::scale_color_carto_d(palette = "TealGrn"),
+  ggplot2.ordinal.fill =  \(...) rcartocolor::scale_color_carto_d(palette = "TealGrn")
 )
 
 ptol_blue = "#4477aa"
@@ -28,4 +31,4 @@ ragg_png <- function(..., res = 150) {
   ragg::agg_png(..., res = res, units = "in")
 }
 
-
+knitr::knit_hooks$set(crop = knitr::hook_pdfcrop)
